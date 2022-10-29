d09e16f194165f716c2e619901b382b3cd1f9c5d
Seth <hyrum.s.white@gmail.com>
2022-10-28 20:37:36 -0400

    updated about me

--- ExtensionContentTypeMap.ts
+++ ExtensionContentTypeMap.ts
@@ -1,5 +1,6 @@
 export const extensionContentTypeMap: Object<"css" | "js" | "html", string> = {
-	".css": "text/css",
-	".js": "application/javascript",
-	".html": "text/html; charset=utf-8",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".html": "text/html; charset=utf-8",
+  ".svg": "image/svg+xml",
 };
